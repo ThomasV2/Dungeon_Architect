8b3651e1f2b4df839a66434366bcf100538b2b55
ThomasV2 <dark.akio@gmail.com>
2015-05-09 19:36:25 +0200

    Chargement de la map en dynamique.

--- Assets/Scripts/Map_Generator.cs
+++ Assets/Scripts/Map_Generator.cs
@@ -7,9 +7,9 @@ public class Map_Generator : MonoBehaviour {
     public GameObject Player_Prefab;
     public GameObject Ground_Prefab;
     public GameObject Finish_Prefab;
+    public GameObject Wall_Prefab;
     public GameObject Camera;
     public GameObject[] item_list;
-    public int Size = 0;
 
     private List<GameObject> Items = new List<GameObject>();
     private GameObject current_player;
@@ -20,19 +20,90 @@ public class Map_Generator : MonoBehaviour {
     private bool isBuild = false;
     private float timer;
     private float velocity = 0f;
-	// Use this for initialization
-	void Start () 
+    private Vector3 pos_begin;
+
+    #region Map
+    enum Tile_Type
     {
-        GameObject ground;
-	    int i;
-        current_player = Instantiate(Player_Prefab);
-        for (i = 0; i < Size; i++)
+        Invalid = -1,
+        Wall,
+        Ground,
+        Begin,
+        Finish
+    };
+
+        int[,] map = new int[20,20] 
+            {
+            {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1},
+            {-1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1},
+            {-1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1},
+            {-1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1},
+            {-1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1},
+            {-1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1},
+            {-1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, -1},
+            {-1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, -1},
+            {-1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, -1},
+            {-1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 3, 0, 0, 0, -1},
+            {-1, 2, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, -1},
+            {-1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, -1},
+            {-1, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, -1},
+            {-1, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, -1},
+            {-1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1},
+            {-1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1},
+            {-1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1},
+            {-1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1},
+            {-1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1},
+            {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1}
+            };
+    #endregion
+
+        // Use this for initialization
+    void Start()
         {
-            ground = Instantiate(Ground_Prefab);
-            ground.transform.position = new Vector3(i * 2.0f, 0, 0);
-        }
-        ground = Instantiate(Finish_Prefab);
-        ground.transform.position = new Vector3(i * 2.0f, 0, 0);
+            GameObject ground;
+	        int y = 0;
+            int x;
+            while (y < 20)
+            {
+                x = 0;
+                while (x < 20)
+                {
+                    switch (map[y,x])
+                    {
+                        case (int)Tile_Type.Invalid:
+                            break;
+
+                        case (int)Tile_Type.Wall:
+                            ground = Instantiate(Wall_Prefab);
+                            ground.transform.position = new Vector3(x, 0, y);
+                            break;
+
+                        case (int)Tile_Type.Ground:
+                            ground = Instantiate(Ground_Prefab);
+                            ground.transform.position = new Vector3(x, 0, y);
+                            break;
+
+                        case (int)Tile_Type.Begin:
+                            ground = Instantiate(Player_Prefab);
+                            ground.transform.position = new Vector3(x, 0, y);
+                            pos_begin = ground.transform.position;
+                            current_player = ground;
+                            ground = Instantiate(Ground_Prefab);
+                            ground.transform.position = new Vector3(x, 0, y);
+                            break;
+
+                        case (int)Tile_Type.Finish:
+                            ground = Instantiate(Finish_Prefab);
+                            ground.transform.position = new Vector3(x, 0, y);
+                            break;
+                        
+                        default:
+                            break;
+                    }
+                    ++x;
+                }
+                ++y;
+            }
         foreach (GameObject item in Items)
         {
             Instantiate(item);
@@ -49,7 +120,7 @@ public class Map_Generator : MonoBehaviour {
         {
             if (Input.GetAxis("Horizontal") > 0.5f)
             {
-                current_x = ((current_x < Size) ? current_x + 1 : current_x);
+                current_x = ((current_x < 20) ? current_x + 1 : current_x);
                 timer = 0f;
             }
             else if (Input.GetAxis("Horizontal") < -0.5f)
@@ -59,15 +130,15 @@ public class Map_Generator : MonoBehaviour {
             }
             else if (Input.GetAxis("Vertical") > 0.5f)
             {
-                current_y = ((current_y < 1) ? current_y + 1 : current_y);
+                current_y = ((current_y < 20) ? current_y + 1 : current_y);
                 timer = 0f;
             }
             else if (Input.GetAxis("Vertical") < -0.5f)
             {
-                current_y = ((current_y > 0) ? current_y - 1 : current_y);
+                current_y = ((current_y > 1) ? current_y - 1 : current_y);
                 timer = 0f;
             }
-            current_item.transform.position = new Vector3(current_x * 2f, 0f, current_y * 2f);
+            current_item.transform.position = new Vector3(current_x, current_item.transform.position.y, current_y);
            if (Input.GetButtonDown("Prev"))
             {
                 index_select = index_select > 0 ? index_select - 1 : item_list.Length - 1;
@@ -84,16 +155,32 @@ public class Map_Generator : MonoBehaviour {
                 Destroy(current_item);
                 current_item = new_item;
             }
-            if (Input.GetButtonDown("Validation"))
+            if (Input.GetButtonDown("Validation") && checkValidation())
             {
-                Debug.Log("Button !");
+                current_item.transform.position = new Vector3(current_item.transform.position.x, 0f, current_item.transform.position.z);
                 Items.Add(current_item);
                 isBuild = false;
                 current_item = null;
                 current_player = Instantiate(Player_Prefab);
+                current_player.transform.position = pos_begin;
             }
         }
         timer += Time.deltaTime;
+    }
+
+    bool checkValidation()
+    {
+        if (map[(int)current_item.transform.position.z, (int)current_item.transform.position.x] != (int)Tile_Type.Ground)
+            return false;
+        foreach (GameObject item in Items)
+        {
+            if ((item.transform.position.x == current_item.transform.position.x
+                && item.transform.position.z == current_item.transform.position.z))
+            {
+                return false;
+            }
+        }
+        return true;
     }
 
     public void PutMode()
@@ -102,5 +189,7 @@ public class Map_Generator : MonoBehaviour {
         isBuild = true;
         index_select = 0;
         current_item = Instantiate(item_list[0]);
+        current_item.transform.position = new Vector3(1f, 0.5f, 1f);
     }
+
 }
---
+++ Assets/knight/KnightAnimationBehavior.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Animator))]
+public class KnightAnimationBehavior : MonoBehaviour
+{
+    private Animator _animator;
+
+    public float bpm
+    {
+        set
+        {
+            _animator.speed = value / 60f;
+        }
+    }
+
+	void Start()
+    {
+        _animator = GetComponent<Animator>();
+
+
+        bpm = 120f;
+	}
+}
