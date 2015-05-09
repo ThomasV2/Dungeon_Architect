6f85e5c9f6fdf1ff65ca10225d18129c23b1b68b
ThomasV2 <dark.akio@gmail.com>
2015-05-09 15:53:41 +0200

    Commencement du mode Build

--- Assets/Scripts/Map_Generator.cs
+++ Assets/Scripts/Map_Generator.cs
@@ -14,6 +14,10 @@ public class Map_Generator : MonoBehaviour {
     private List<GameObject> Items = new List<GameObject>();
     private GameObject current_player;
     private GameObject current_item;
+    private int current_x = 1;
+    private int current_y = 0;
+    private bool isBuild = false;
+    private float timer;
 	// Use this for initialization
 	void Start () 
     {
@@ -39,11 +43,38 @@ public class Map_Generator : MonoBehaviour {
             Camera.transform.position = new Vector3(current_player.transform.position.x + 2f, current_player.transform.position.y + 7.5f, current_player.transform.position.z + -7.5f);
         else
             Camera.transform.position = new Vector3(current_item.transform.position.x + 2f, current_item.transform.position.y + 7.5f, current_item.transform.position.z + -7.5f);
+        if (isBuild && timer > 0.1f)
+        {
+            if (Input.GetAxis("Horizontal") > 0.5f)
+            {
+                current_x = ((current_x < Size) ? current_x + 1 : current_x);
+                timer = 0f;
+            }
+            else if (Input.GetAxis("Horizontal") < -0.5f)
+            {
+                current_x = ((current_x > 1) ? current_x - 1 : current_x);
+                timer = 0f;
+            }
+            else if (Input.GetAxis("Vertical") > 0.5f)
+            {
+                current_y = ((current_y < 1) ? current_y + 1 : current_y);
+                timer = 0f;
+            }
+            else if (Input.GetAxis("Vertical") < -0.5f)
+            {
+                current_y = ((current_y > 0) ? current_y - 1 : current_y);
+                timer = 0f;
+            }
+            current_item.transform.position = new Vector3(current_x * 2f, 0f, current_y * 2f);
+        }
+        timer += Time.deltaTime;
     }
 
     public void PutMode()
     {
         Destroy(current_player);
-
+        isBuild = true;
+        // TODO modifier la valeur de 0 en index i variable
+        current_item = Instantiate(item_list[0]);
     }
 }
