8850d58cbb7b0259fd040fdf070b179a6a9d8a87
ThomasV2 <dark.akio@gmail.com>
2015-05-10 12:03:24 +0200

    Ajout d'un ecran d'intro + d'un timer pour le score + on attend que le joueur appuit sur une touche pour relancer

---
+++ Assets/Scripts/Intro.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Intro : MonoBehaviour {
+
+    public Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        anim.SetFloat("walking", 1f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	if (Input.GetButtonDown("Validation"))
+    {
+        Application.LoadLevel("Main");
+    }
+	}
+}
--- Assets/Scripts/Map_Generator.cs
+++ Assets/Scripts/Map_Generator.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
 
@@ -8,6 +9,7 @@ public class Map_Generator : MonoBehaviour {
     public GameObject[] Prefab;
     public GameObject Camera;
     public List<Tile_Type> item_list;
+    public Text time_score;
 
     private List<GameObject> Items = new List<GameObject>();
     private GameObject current_player;
@@ -16,10 +18,10 @@ public class Map_Generator : MonoBehaviour {
     private int current_x = 1;
     private int current_y = 0;
     private bool isBuild = false;
+    private bool isWait = false;
     private float timer;
     private float velocity = 0f;
-    private Vector3 pos_begin;
-
+    private float score;
     private GameObject parent;
     #region Map
     public enum Tile_Type
@@ -70,6 +72,7 @@ public class Map_Generator : MonoBehaviour {
     {
         parent = new GameObject();
         parent.name = "Parent";
+        score = 60f;
         GameObject ground;
         int y = 0;
         int x;
@@ -101,7 +104,6 @@ public class Map_Generator : MonoBehaviour {
                     ground = Instantiate(Prefab[(int)Tile_Type.Begin]);
                     ground.transform.position = new Vector3(x, 0, y);
                     ground.transform.parent = parent.transform;
-                    pos_begin = ground.transform.position;
                     current_player = ground;
                     ground = Instantiate(Prefab[(int)Tile_Type.Ground]);
                     ground.transform.position = new Vector3(x, 0, y);
@@ -131,69 +133,87 @@ public class Map_Generator : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (current_player != null)
-            Camera.transform.position = new Vector3(current_player.transform.position.x + 2f, current_player.transform.position.y + 7.5f, current_player.transform.position.z + -7.5f);
-        else if (current_item != null)
-            Camera.transform.position = new Vector3(Mathf.SmoothDamp(Camera.transform.position.x, current_item.transform.position.x + 2f, ref velocity, 0.1f), Mathf.SmoothDamp(Camera.transform.position.y, current_item.transform.position.y + 7.5f, ref velocity, 0.1f), Mathf.SmoothDamp(Camera.transform.position.z, current_item.transform.position.z + -7.5f, ref velocity, 0.1f));
-        if (isBuild && timer > 0.1f)
+        if (isWait)
         {
-            if (Input.GetAxis("Horizontal") > 0.5f)
+            time_score.text = "Please, change the Player and press A";
+            if (Input.GetButtonDown("Validation"))
             {
-                current_x = ((current_x < 20) ? current_x + 1 : current_x);
-                timer = 0f;
+                current_item = null;
+                Destroy(parent.gameObject);
+                Generate();
+                isWait = false;
             }
-            else if (Input.GetAxis("Horizontal") < -0.5f)
+
+        }
+        else
+        {
+            if (current_player != null)
+                Camera.transform.position = new Vector3(current_player.transform.position.x + 2f, current_player.transform.position.y + 7.5f, current_player.transform.position.z + -7.5f);
+            else if (current_item != null)
+                Camera.transform.position = new Vector3(Mathf.SmoothDamp(Camera.transform.position.x, current_item.transform.position.x + 2f, ref velocity, 0.1f), Mathf.SmoothDamp(Camera.transform.position.y, current_item.transform.position.y + 7.5f, ref velocity, 0.1f), Mathf.SmoothDamp(Camera.transform.position.z, current_item.transform.position.z + -7.5f, ref velocity, 0.1f));
+            if (isBuild && timer > 0.1f)
             {
-                current_x = ((current_x > 1) ? current_x - 1 : current_x);
-                timer = 0f;
-            }
-            else if (Input.GetAxis("Vertical") > 0.5f)
-            {
-                current_y = ((current_y < 20) ? current_y + 1 : current_y);
-                timer = 0f;
-            }
-            else if (Input.GetAxis("Vertical") < -0.5f)
-            {
-                current_y = ((current_y > 1) ? current_y - 1 : current_y);
-                timer = 0f;
-            }
-            current_item.transform.position = new Vector3(current_x, current_item.transform.position.y, current_y);
-           if (Input.GetButtonDown("Prev"))
-            {
-                index_select = index_select > 0 ? index_select - 1 : item_list.Count - 1;
-                GameObject new_item = Instantiate(Prefab[(int)item_list[index_select]]);
-                new_item.transform.position = current_item.transform.position;
-                Destroy(current_item);
-                current_item = new_item;
-            }
-            else if (Input.GetButtonDown("Next"))
-            {
-                index_select = index_select < item_list.Count - 1 ? index_select + 1 : 0;
-                GameObject new_item = Instantiate(Prefab[(int)item_list[index_select]]);
-                new_item.transform.position = current_item.transform.position;
-                Destroy(current_item);
-                current_item = new_item;
-            }
-            if (Input.GetButtonDown("Validation") && checkValidation())
-            {
-                current_item.transform.position = new Vector3(current_item.transform.position.x, 0f, current_item.transform.position.z);
-                // TODO mettre comme valeur (index_select + valeur) le nombre de tile qui ne sont pas des item_trap - 1 (à cause de l'invalide).
-                map[(int)current_item.transform.position.z, (int)current_item.transform.position.x] = (int)item_list[index_select];
-                item_list.RemoveAt(index_select);
-                Debug.Log("test");
-                if (item_list.Count <= 0)
+                if (Input.GetAxis("Horizontal") > 0.5f)
                 {
-                    Debug.Log("ok");
-                    isBuild = false;
-                    current_item = null;
-                    Destroy(parent.gameObject);
-                    Generate();
+                    current_x = ((current_x < 20) ? current_x + 1 : current_x);
+                    timer = 0f;
                 }
-                else
-                    current_item = Instantiate(Prefab[(int)item_list[0]]);
+                else if (Input.GetAxis("Horizontal") < -0.5f)
+                {
+                    current_x = ((current_x > 1) ? current_x - 1 : current_x);
+                    timer = 0f;
+                }
+                else if (Input.GetAxis("Vertical") > 0.5f)
+                {
+                    current_y = ((current_y < 20) ? current_y + 1 : current_y);
+                    timer = 0f;
+                }
+                else if (Input.GetAxis("Vertical") < -0.5f)
+                {
+                    current_y = ((current_y > 1) ? current_y - 1 : current_y);
+                    timer = 0f;
+                }
+                current_item.transform.position = new Vector3(current_x, current_item.transform.position.y, current_y);
+                if (Input.GetButtonDown("Prev"))
+                {
+                    index_select = index_select > 0 ? index_select - 1 : item_list.Count - 1;
+                    GameObject new_item = Instantiate(Prefab[(int)item_list[index_select]]);
+                    new_item.transform.position = current_item.transform.position;
+                    Destroy(current_item);
+                    current_item = new_item;
+                }
+                else if (Input.GetButtonDown("Next"))
+                {
+                    index_select = index_select < item_list.Count - 1 ? index_select + 1 : 0;
+                    GameObject new_item = Instantiate(Prefab[(int)item_list[index_select]]);
+                    new_item.transform.position = current_item.transform.position;
+                    Destroy(current_item);
+                    current_item = new_item;
+                }
+                if (Input.GetButtonDown("Validation") && checkValidation())
+                {
+                    current_item.transform.position = new Vector3(current_item.transform.position.x, 0f, current_item.transform.position.z);
+                    map[(int)current_item.transform.position.z, (int)current_item.transform.position.x] = (int)item_list[index_select];
+                    item_list.RemoveAt(index_select);
+                    if (item_list.Count <= 0)
+                    {
+                        isBuild = false;
+                        isWait = true;
+                        /*current_item = null;
+                        Destroy(parent.gameObject);
+                        Generate();*/
+                    }
+                    else
+                        current_item = Instantiate(Prefab[(int)item_list[0]]);
+                }
+            }
+            timer += Time.deltaTime;
+            if (!isBuild)
+            {
+                score -= Time.deltaTime;
+                time_score.text = score.ToString("0");
             }
         }
-        timer += Time.deltaTime;
     }
 
     bool checkValidation()
@@ -216,15 +236,7 @@ public class Map_Generator : MonoBehaviour {
 		Debug.Log("Contact Bumper !");
 		if (map[(int)destination.z, (int)destination.x] == (int)Tile_Type.Wall)
 			return false;
-/*		foreach (GameObject item in Items)
-		{
-			if ((   item.transform.position.x == destination.x
-			     && item.transform.position.z == destination.z))
-			{
-				return false;
-			}
-		}
-*/		return true;
+		return true;
 	}
 
     public void PutMode()
@@ -250,10 +262,12 @@ public class Map_Generator : MonoBehaviour {
 		else
         {
             isBuild = false;
-            current_item = null;
+            isWait = true;
+            /*current_item = null;
             Destroy(parent.gameObject);
-            Generate();
+            Generate();*/
         }
     }
+
 
 }
